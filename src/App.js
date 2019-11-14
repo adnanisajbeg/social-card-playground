0a7dc4099203478defe112e045092a7511219930
Adnan Isajbegovic <adnan.isajbegovic@gmail.com>
2019-11-14 10:54:59 +0100

    new structure and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const testData = [
-  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-];
-
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
+import Form from './components/Form';
+import Card from './components/Card';
 
 const CardList = (props) => (
     <div>
-      {testData.map(profile => <Card {...profile}/>)}
+      {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
     </div>
 );
 
-class Card extends React.Component {
+class App extends React.Component {
+  state = {
+    profiles: [],
+  };
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData],
+    }));
+  };
   render() {
-    const profile = this.props;
     return (
-        <div className="github-profile">
-          <img src={profile.avatar_url} />
-          <div className="info">
-            <div className="name">{profile.name}</div>
-            <div className="company">{profile.company}</div>
-          </div>
+        <div>
+          <div className="header">{this.props.title}</div>
+          <Form onSubmit={this.addNewProfile} />
+          <CardList profiles={this.state.profiles} />
         </div>
     );
   }
-}
-function App() {
-  return (
-    <div className="App">
-      <div className="header">Test project - Github social card</div>
-      <CardList />
-    </div>
-  );
 }
 
 export default App;
